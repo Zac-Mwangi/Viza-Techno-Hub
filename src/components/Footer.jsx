7939534fa77a57d60c613e4e6927c990c7e09e1b
Zac-Mwangi <zackmwangi998@gmail.com>
2022-07-26 12:34:53 +0300

    add footer for the project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Typography } from '@mui/material'
+import useStyles from '../styles'
+
+export default function Footer() {
+    const classes = useStyles()
+
+    return (
+        <footer className={classes.footer}>
+            <Typography variant='h6' align='center' gutterBottom>
+                Footer
+            </Typography>
+            <Typography variant='subtitle1' align='center' color='textSecondary'>
+                Something here to give the footer a purpose</Typography>
+        </footer>
+    )
+}
